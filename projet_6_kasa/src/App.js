4eb058bd224e6cba3044c92e8e0d5e7462dae80f
Julianne Duhaze <jduhaze@protonmail.com>
2024-02-08 15:23:18 +0100

    feat: add housing route

--- projet_6_kasa/src/App.js
+++ projet_6_kasa/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import data from './housing.json';
 import About from "./pages/About.page";
 import HomePage from "./pages/Home.page";
+import Housing from "./pages/Housing.page";
 // import { useState } from "react";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/about_us' element={<About />}/>
+        <Route path='/housing' element={<Housing />}/>
         <Route path='*' element={<Navigate to='/' replace={false} />} />
       </Routes>
     </BrowserRouter>
