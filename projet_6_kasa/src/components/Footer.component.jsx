fd6670fa31d2aa6d3bf50e318adf16224f5507f4
Julianne Duhaze <jduhaze@protonmail.com>
2024-02-21 23:26:20 +0100

    style: add a .title selector

--- projet_6_kasa/src/components/Footer.component.jsx
+++ projet_6_kasa/src/components/Footer.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer' >
-      <p>
+      <p className='title'>
         K
         <svg viewBox='0 0 32 38' className='logo footer__logo' xmlns='http://www.w3.org/2000/svg'>
           <path d="M31.7618 14.6403L16.6524 5.8262L8.27235 0.972076L0.0192871 15.4067L0.146255 28.9471L15.1287 37.7612L16.6524 36.867V24.093L23.3818 12.2132L30.2382 16.1731V28.9471L31.7618 28.053V14.6403Z"/>
