5108d0334a901f214346bd0971a7da637c0e3175
Julianne Duhaze <jduhaze@protonmail.com>
2024-02-22 15:34:59 +0100

    feat: housing tags + style

--- projet_6_kasa/src/components/HousingTag.component.jsx
+++ projet_6_kasa/src/components/HousingTag.component.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'; 
 
-export default function HousingTag() {
 
-  return (
-    <div className='housing-tag' >
- 
-    </div>
-  )
+export default function HousingTag({ tags }) { 
+    return (
+        <div className="tags">
+            {tags.map((tag, index) => (
+                <button key={index} className="btn tag">{tag}</button>
+            ))}
+        </div>
+    );
 }
+
+HousingTag.propTypes = {
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
