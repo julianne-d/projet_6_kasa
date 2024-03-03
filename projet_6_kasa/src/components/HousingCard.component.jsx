477f6d890b73727adea60cb44dd9a89981d14aed
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-03 20:18:44 +0100

    misc: removed import of scss file in the component file

--- projet_6_kasa/src/components/HousingCard.component.jsx
+++ projet_6_kasa/src/components/HousingCard.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 import { Link } from 'react-router-dom';
 
-import '../sass/components/_housingCard.component.scss';
+// import '../sass/components/_housingCard.component.scss';
 export default function HousingCard({ house }) {
   return (
     <Link className='link' to={`/housing/${house.id}`}>
       <div className='housing-card'  
-        style={{ 
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url(${house.cover})`
+        style={{
+          background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url(${house.cover}) no-repeat center center / cover`,
         }}>
         <h2>{house.title}</h2>
     </div>
