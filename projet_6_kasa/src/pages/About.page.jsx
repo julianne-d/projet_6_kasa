ebe517e9d9d4d6b29200fc4acc70018e68a4f812
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-06 17:33:43 +0100

    fix: async helmet librarymargin (mobile) and collapse-menu gap (housing page)

--- projet_6_kasa/src/pages/About.page.jsx
+++ projet_6_kasa/src/pages/About.page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import Header from '../components/Header.component';
 import Footer from '../components/Footer.component';
 import Banner from '../components/Banner.component';
--- projet_6_kasa/src/pages/Home.page.jsx
+++ projet_6_kasa/src/pages/Home.page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import Header from '../components/Header.component';
 import Banner from '../components/Banner.component';
 import CardsContainer from '../components/CardsContainer.component';
