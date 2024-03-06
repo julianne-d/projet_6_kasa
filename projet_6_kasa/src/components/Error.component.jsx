c8e427d91003695001ff47f24c52481ac34f1d5d
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-06 19:42:58 +0100

    fix: style of error component

--- projet_6_kasa/src/components/Error.component.jsx
+++ projet_6_kasa/src/components/Error.component.jsx
@@ -8,15 +8,17 @@ import Footer from '../components/Footer.component';
 export default function Error() {
     return (
         <>
-            <Helmet>
-                <title>404</title>
-            </Helmet>
-            
-            <Header />
-            <div className="error-404-container">
-                <h1 className="error-404-container__h1">404</h1>
-                <p className="error-404-container__text">Oups ! La page que <br className='hidden-on-desktop'/> vous demandez n'existe pas.</p>
-                <Link className='link error-404-container__link' to='/about'>Retourner sur la page d'accueil</Link>
+            <div className='page'>
+                <Helmet>
+                    <title>404</title>
+                </Helmet>
+                
+                <Header />
+                <div className="error-404-container">
+                    <h1 className="error-404-container__h1">404</h1>
+                    <p className="error-404-container__text">Oups ! La page que <br className='hidden-on-desktop'/> vous demandez n'existe pas.</p>
+                    <Link className='link error-404-container__link' to='/'>Retourner sur la page d'accueil</Link>
+                </div>
             </div>
             <Footer />
         </>
